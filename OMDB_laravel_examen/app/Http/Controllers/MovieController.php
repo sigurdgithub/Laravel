8ba989349b3ab4c273aa3733785b857e7df225db
Sigurd B <sigurdboels96@hotmail.com>
2022-05-30 14:05:49 +0200

    Migration & routing

---
+++ OMDB_laravel_examen/app/Http/Controllers/MovieController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MovieController extends Controller
+{
+    public function search(Request $request)
+    {
+    }
+}
--- OMDB_laravel_examen/routes/web.php
+++ OMDB_laravel_examen/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,13 +14,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// WELCOME ROUTES --------------------------------
 Route::get('/', function () {
     return view('welcome');
 });
 
+// DASHBOARD ROUTE --------------------------------
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+// GET MOVIE ROUTE --------------------------------
+Route::get('/movies', function () {
+    return view('movie');
+});
+
+// POST MOVIE API --------------------------------
+Route::post('/movies', function (Request $request) {
+    $search = $request->input('movie');
+    $movies = Http::get('http://www.omdbapi.com/?i=tt3896198&apikey=f45f2e0c' . $search);
+    $movies = json_decode($movies);
+    return view('movie', compact('movies'));
+});
+
+
+
+require __DIR__ . '/auth.php';
